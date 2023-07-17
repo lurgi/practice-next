d11197ec8e5aefa00861f2087c6b3a49c8426664
lurgi <wahoo9040@naver.com>
2023-07-17 16:02:07 +0900

    set font

--- app/hi/page.tsx
+++ app/hi/page.tsx
@@ -2,11 +2,11 @@
 
 import { useState } from "react";
 
-export default function hi() {
+export default function Hi() {
   const [state, setState] = useState(0);
   return (
     <>
-      <h1>here is secret hi</h1>
+      <h1 className="font-roboto">here is secret hi</h1>
       <span>{state}</span>
       <button onClick={() => setState((prev) => prev + 1)}>click</button>
     </>
