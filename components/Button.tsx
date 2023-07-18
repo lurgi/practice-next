345972afb37c70a110b401c918875010e184e9b9
lurgi <wahoo9040@naver.com>
2023-07-19 08:47:23 +0900

    header button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,26 @@
-const Button = () => {
-  return <div>Button</div>;
-};
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, IButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        type={type}
+        disabled={disabled}
+        className={twMerge(
+          `w-full rounded-full border border-transparent bg-emerald-500 p-3 disabled:cursor-not-allowed disabled:opacity-75 text-black font-bold`,
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 export default Button;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,39 @@
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import { AiFillHome } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
 
-const Header = () => {
+const Header = ({ children }: { children: React.ReactNode }) => {
   return (
     <header className={twMerge("bg-gradient-to-b from-emerald-800 h-fit p-6")}>
       <div className="flex justify-between items-center mb-4">
         <div className="flex gap-x-4">
-          <button className="rounded-full p-1.5 bg-neutral-950 flex justify-center items-center">
+          <button className="hidden transition md:flex rounded-full p-1.5 bg-neutral-950 hover:opacity-75 justify-center items-center">
             <FiChevronLeft size={25} />
           </button>
-          <button className="rounded-full p-1.5 bg-neutral-950 flex justify-center items-center">
+          <button className="hidden transition md:flex rounded-full p-1.5 bg-neutral-950 hover:opacity-75 justify-center items-center">
             <FiChevronRight size={25} />
           </button>
+          <button className="flex md:hidden transition rounded-full p-2 bg-neutral-50 hover:opacity-75 justify-center items-center ">
+            <AiFillHome size={21} className="text-neutral-900" />
+          </button>
+          <button className="flex md:hidden transition rounded-full p-2 bg-neutral-50 hover:opacity-75 justify-center items-center">
+            <BiSearch size={21} className="text-neutral-900" />
+          </button>
         </div>
-        <Button />
+        <div className="flex gap-x-4">
+          <div>
+            <Button className="bg-transparent text-neutral-300 font-medium">
+              가입하기
+            </Button>
+          </div>
+          <div>
+            <Button className="py-3 px-6">로그인</Button>
+          </div>
+        </div>
       </div>
+      <div>{children}</div>
     </header>
   );
 };
